ee8c9d0da23ecfcc940cbec8adc592e205a23bec
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-07-12 16:19:53 +0300

    проверка минимальной длины поля

---
+++ rules/min.go
@@ -0,0 +1,23 @@
+package rules
+
+import (
+	"errors"
+	"fmt"
+	"github.com/algrvvv/validator/types"
+)
+
+func Min(f, tag, v string, m types.IMessages) error {
+	var minValue int
+	_, err := fmt.Sscanf(tag, "min=%d", &minValue)
+	if err != nil {
+		// паникуем, чтобы недопустить использование
+		// неправильного минимального значения для работы
+		panic("ошибка чтения минимального значения")
+	}
+
+	if minValue > len(v) {
+		return errors.New(m.Min(f, minValue))
+	}
+
+	return nil
+}
